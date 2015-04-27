ad03be6c36f9aa94d8310e9f86137c2fae8c4658
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2015-04-27 18:29:54 +0200

    Added a very basic interpreter skeleton.

--- Main.hs
+++ Main.hs
@@ -1,6 +1,28 @@
 {- Tomasz Zakrzewski, tz336079
  - New Better Language Interpreter
  -}
+module Main where
 
-main :: IO ()
-main = print "NBL interpreter v0.1"
+
+import LexNBL
+import ParNBL
+import SkelNBL
+import PrintNBL
+import AbsNBL
+import ErrM
+
+
+interpret :: Program -> String
+interpret e = "TODO"
+
+
+run :: String -> (String, Integer)
+run s = case pProgram (myLexer s) of
+    Bad err -> ("Parsing error: " ++ err, -1)
+    Ok e -> (interpret e, 0)	-- TODO
+
+
+main = do
+  code <- getContents
+  let (out, ret) = run code
+  putStrLn $ out
