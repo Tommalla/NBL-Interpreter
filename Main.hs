1e16f337709f2f3285e5c6c22f6fc9efb87b4ecd
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2015-05-11 03:07:36 +0200

    Added proper error handling.

--- Main.hs
+++ Main.hs
@@ -3,6 +3,7 @@
  -}
 module Main where
 
+import Control.Monad.Except
 import Control.Monad.Trans.State
 import Data.Map
 import Data.Maybe
@@ -17,8 +18,15 @@ import ErrM
 import TypeChecking
 
 
-data DataType = TVoid | TChar Char | TInt Integer | TDouble Double | TBool Bool | TString String | TPointer DataType | 
-		TConst DataType
+data DataType = 
+	  TVoid 
+	| TChar Char 
+	| TInt Integer 
+	| TDouble Double 
+	| TBool Bool 
+	| TString String 
+	| TPointer DataType 
+	| TConst DataType
 	deriving (Show)
 
 data ParseResult = ParseOk | Error String
@@ -27,8 +35,8 @@ type Loc = Int
 type Env = Map Ident Loc
 type MemState = Map Loc DataType
 -- type PEnv = Map Ident TODO
-type GlobalState = State (Env, MemState) ParseResult
-type ExpState = State (Env, MemState) Exp
+type GlobalState = ExceptT String (State (Env, MemState)) ParseResult
+type ExpState = ExceptT String (State (Env, MemState)) Exp
 
 
 -- Returns a new location
@@ -60,20 +68,20 @@ createDefaultValue (h:t) = if (not (Prelude.null t)) then
 
 allocateDirect :: DirectDeclarator -> [DeclarationSpecifier] -> GlobalState
 allocateDirect (Name ident) specifiers = do
-	(env, state) <- get
+	(env, state) <- lift $ get
 	-- FIXME conflicts?
 	let loc = newloc state
-	put (insert ident loc env, insert loc (createDefaultValue specifiers) state)
+	lift $ put (insert ident loc env, insert loc (createDefaultValue specifiers) state)
 	return ParseOk
-allocateDirect _ _ = return (Error "Allocation not supported")
+allocateDirect _ _ = throwError "This type of allocation is not supported yet."
 
 
 allocateDeclarator :: InitDeclarator -> [DeclarationSpecifier] -> GlobalState
 allocateDeclarator (PureDecl declarator) specifiers = do
 	case declarator of
 		NoPointer directDeclarator -> allocateDirect directDeclarator specifiers
-		_ -> return (Error "Not a NoPointer")
-allocateDeclarator _ _ = return (Error "undefined")
+		_ -> throwError "Not a NoPointer"
+allocateDeclarator _ _ = throwError "Allocate declarator not defined for this type of declaration."
 
 
 interpretExp :: Exp -> ExpState
@@ -82,22 +90,23 @@ interpretExp (ExpAssign exp1 assignmentOperator exp2) = do
 	res2 <- interpretExp exp2
 	case res1 of
 		ExpVar ident -> do
-				(mem, state) <- get
+				(mem, state) <- lift $ get
 				let val = case res2 of 
 					-- TODO pointers
 					ExpConstant (ExpInt v) -> TInt v
 					_ -> undefined
 				let loc = getLoc ident mem
 				if (isNothing loc) then
-					undefined
+					let (Ident identStr) = ident in
+							throwError ("Exp: " ++ identStr ++ " was not declared!")
 				else 
-					put (mem, update (\_ -> Just val) (fromJust loc) state)
+					lift $ put (mem, update (\_ -> Just val) (fromJust loc) state)
 				return res2
-		_ -> undefined -- Not an lvalue
+		_ -> throwError "Trying to assign to something that isn't an lvalue!"
 	-- TODO handle all sorts of different assignment operators
 interpretExp (ExpVar ident) = return (ExpVar ident)
 interpretExp (ExpConstant constant) = return (ExpConstant constant)
-interpretExp _ = undefined
+interpretExp _ = throwError "This type of exception is not supported yet."
 
 
 interpretStmt :: Stmt -> GlobalState
@@ -105,16 +114,17 @@ interpretStmt :: Stmt -> GlobalState
 interpretStmt (DeclS (Declarators specifiers initDeclarators)) = allocateDeclarator (head initDeclarators) specifiers
 interpretStmt (ExprS (ExtraSemicolon)) = return ParseOk
 interpretStmt (ExprS (HangingExp exp)) = do
-	mem <- get
-	put (snd (runState (interpretExp exp) mem))
+	mem <- lift $ get
+	lift $ put (snd (runState (runExceptT (interpretExp exp)) mem))
 	return ParseOk
 interpretStmt (CompS (StmtComp statements)) = do
-	results <- mapM (interpretStmt) statements
-	if (all (== ParseOk) results) then
-		return ParseOk
-	else
-		return (Error "Some error")
-interpretStmt _ = return (Error "undefined statement")
+	mapM_ (interpretStmt) statements
+	return ParseOk
+interpretStmt _ = throwError "This type of statement is not supported yet."
+
+
+interpretProg :: Prog -> GlobalState
+interpretProg (Program externalDeclarations) = undefined
 
 
 -- TODO change run to work on programs - need standard entry point and functions.
@@ -123,9 +133,9 @@ run :: String -> (String, (Env, MemState))
 run s = case pStmt (myLexer s) of
     Bad err -> ("Parsing error: " ++ err, (empty, empty))
     -- TODO (fix below)
-    Ok p -> case (runState (interpretStmt p) (empty, empty)) of
-    	(ParseOk, mem) -> ("Run successful", mem)
-    	(Error str, mem) -> ("Runtime error: " ++ str, mem)
+    Ok p -> case (runState (runExceptT (interpretStmt p)) (empty, empty)) of
+    	((Right _), mem) -> ("Run successful", mem)
+    	((Left str), mem) -> ("Runtime error: " ++ str, mem)
 
 
 main = do
